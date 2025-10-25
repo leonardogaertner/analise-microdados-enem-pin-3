d46be28fc1ea1ab45bfb3462c94fa1459b7686d0
leonardogaertner <leonardogaertner2004@gmail.com>
2025-10-25 13:20:51 -0300

    adição da tabela de municipios

--- pages/1_Exploration.py
+++ pages/1_Exploration.py
@@ -36,7 +36,7 @@ DB_HOST = os.environ.get('DB_HOST', '127.0.0.1')
 DB_PORT = os.environ.get('DB_PORT', '5432')
 DB_NAME = os.environ.get('DB_NAME', 'microdados')
 DB_USER = os.environ.get('DB_USER', 'postgres')
-DB_PASS = os.environ.get('DB_PASS', 'admin')
+DB_PASS = os.environ.get('DB_PASS', 'aluno')
 
 # Conexão com o banco
 @st.cache_resource
